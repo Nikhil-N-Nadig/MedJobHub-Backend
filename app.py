f103ddcabe3e33d5dcecbfe9e6471276711a9bab
Nikhil N Nadig <nikhilnadig24@gmail.com>
2025-05-23 12:27:51 +0530

    Removed Chat_Message

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from medjobhub import app,db
 from medjobhub.models import User
-from medjobhub.routes.chat_message import socketio
 if __name__ == '__main__':
     with app.app_context():
         # db.drop_all()
@@ -12,4 +11,4 @@ if __name__ == '__main__':
             user.is_verified = False
         db.session.commit()
 
-    socketio.run(app, debug=True, port=5001)
+    app.run(app, debug=True, port=5001)
